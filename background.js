ec984b901f942cd62d8211ec51f63563b392324f
nfl0 <nawfalona@gmail.com>
2020-10-27 18:39:20 +0800

    Added display mode toggle
    * The price display on the icon can now be toggled between Abbreviated (13k) and Four digit value (1317)
    * Bug fixes and performance improvements

--- background.js
+++ background.js
@@ -1,11 +1,33 @@
-(function () {
+var localStore = browser.storage.local;
+(function() {
+    var price = 0;
+    var prevPrice = 0;
+    var up = true;
+
+    var toggle = false;
+
+
     function formatPrice(price) {
-        var ranges = [
-            { divider: 1e15, suffix: 'Q' },
-            { divider: 1e12, suffix: 'T' },
-            { divider: 1e9, suffix: 'B' },
-            { divider: 1e6, suffix: 'M' },
-            { divider: 1e3, suffix: 'K' }
+        var ranges = [{
+                divider: 1e15,
+                suffix: 'Q'
+            },
+            {
+                divider: 1e12,
+                suffix: 'T'
+            },
+            {
+                divider: 1e9,
+                suffix: 'B'
+            },
+            {
+                divider: 1e6,
+                suffix: 'M'
+            },
+            {
+                divider: 1e3,
+                suffix: 'K'
+            }
         ];
         for (var i = 0; i < ranges.length; i++) {
             if (price >= ranges[i].divider) {
@@ -24,8 +46,43 @@
         }
         return price.toString();
     }
+
+
+
+    function changeMode() {
+        console.log(toggle);
+        switch (toggle) {
+            case true:
+                localStore.set({
+                    ["toggle"]: false
+                });
+                toggle = false;
+                break;
+            case false:
+                localStore.set({
+                    ["toggle"]: true
+                });
+                toggle = true;
+                break;
+            default:
+                localStore.set({
+                    ["toggle"]: true
+                });
+                toggle = true;
+                break;
+        }
+
+        updateBadgeText(price);
+
+    }
+
     function updateBadgeText(price) {
-        var badgeText = formatPrice(price);
+        var badgeText;
+        if (toggle) {
+            badgeText = String(price);
+        } else {
+            badgeText = formatPrice(price);
+        }
         chrome.browserAction.setBadgeText({
             text: badgeText
         });
@@ -33,36 +90,59 @@
             title: String(price)
         });
     }
+
+
     function updateBadge() {
+        //get toggle state
+        localStore.get().then((item) => toggle = item.toggle);
+
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd", true);
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 var response = JSON.parse(xhr.responseText);
-                var price = response.bitcoin.usd;
+                price = response.bitcoin.usd;
                 updateBadgeText(price);
             }
+            if (price >= prevPrice) {
+                setupBadge(up);
+            } else {
+                setupBadge(!up);
+            }
+            prevPrice = price;
         };
         xhr.send();
     }
+
     function setupInterval() {
-        window.setInterval(function () {
+        window.setInterval(function() {
             updateBadge();
-        }, 600000);
+        }, 10000);
     }
-    function setupBadge() {
+
+    function setupBadge(up) {
+        if (up) {
+            setTimeout(function() {
+                chrome.browserAction.setBadgeBackgroundColor({
+                    color: "#009E73"
+                });
+            }, 1000);
+        } else {
+            setTimeout(function() {
+                chrome.browserAction.setBadgeBackgroundColor({
+                    color: "#ff0000"
+                });
+            }, 1000);
+        }
         chrome.browserAction.setBadgeBackgroundColor({
-            color: "#F7931A"
+            color: "#f9a43f"
         });
 
-        chrome.browserAction.setBadgeText({
-            text: "0"
-        });
-		chrome.browserAction.onClicked.addListener(function(){
-		setupInterval();}
-		);
     }
-    setupBadge();
-	updateBadge();
+
+    chrome.browserAction.onClicked.addListener(function(tab) {
+        changeMode();
+    });
+    updateBadge();
     setupInterval();
 })();
